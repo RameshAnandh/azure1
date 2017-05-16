737ad1b0d688af2be0c23606b603c98626132f7e
Ramesh Anandh <rameshdev55@gmail.com>
2017-05-16 20:35:45 +0530

    v1

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAxesrfr8opV-pFfuOphg2TTQi2b4IKJ9Y"></script>
+  <%--  <script type="text/javascript">
+    var markers = [
+    <asp:Repeater ID="rptMarkers" runat="server">
+    <ItemTemplate>
+             {
+                "title": '<%# Eval("Mobile") %>',
+                "lat": '<%# Eval("Latitude") %>',
+                "lng": '<%# Eval("Longitude") %>',
+                "description": '<%# Eval("Date") %>'
+            }
+    </ItemTemplate>
+    <SeparatorTemplate>
+        ,
+    </SeparatorTemplate>
+    </asp:Repeater>
+    ];
+    </script>--%>
+    <img src="Images/Doc.jpg" style="height: 223px; width: 325px" /><img 
+        src="Images/head.jpg" style="height: 220px; width: 594px" />
+    <center>
+    <b>
+    <h3>
+    <div>
+     <asp:GridView ID="GridView1" runat="server" AllowPaging="True" PageSize="10" 
+            onpageindexchanging="GridView1_PageIndexChanging" 
+           >
+            
+        </asp:GridView>
+    
+    </div>
+    </h3>
+    </b>
+    </center>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public static string conn = "server=b1cd7557-6515-4315-b173-a41000718d78.mysql.sequelizer.com;User Id=qhjmurvgkjyckfli;Password=igJW4areBPSwLjbX3JVDAbLwAKKBhG5z3mME5hFL8sai7ALovjkWz73gCnurwW53;Persist Security Info=True;database=dbb1cd755765154315b173a41000718d78";
+    MySqlConnection c1 = new MySqlConnection(conn);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+        //    DataTable dt = this.GetData("select Mobile as HeartRate,Latitude as Temperature from sample");
+          //  rptMarkers.DataSource = dt;
+          //  rptMarkers.DataBind();
+            bindgrid("select Mobile as HeartRate,Latitude as Temperature,Longitude as BloodPressure,DateTime from sample");            
+        }
+    }
+    public void bindgrid(string rr)
+    {
+        try
+        {
+            DataSet dt = new DataSet();
+            c1.Open();
+            MySqlDataAdapter da = new MySqlDataAdapter(rr, c1);
+            da.Fill(dt);
+            GridView1.DataSource = dt.Tables[0];
+            GridView1.DataBind(); da.Dispose();
+        }
+        catch (Exception er)
+        {
+        }
+        finally
+        {
+            c1.Close();
+        }
+    }
+    //private DataTable GetData(string query)
+    //{
+    //    string conString = conn;
+    //    MySqlCommand cmd = new MySqlCommand(query);
+    //    using (MySqlConnection con = new MySqlConnection(conString))
+    //    {
+    //        using (MySqlDataAdapter sda = new MySqlDataAdapter())
+    //        {
+    //            cmd.Connection = con;
+    //            sda.SelectCommand = cmd;
+    //            using (DataTable dt = new DataTable())
+    //            {
+    //                sda.Fill(dt);
+    //                return dt;
+    //            }
+    //        }
+    //    }
+    //}
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        bindgrid("select Mobile as HeartRate,Latitude as Temperature,Longitude as BloodPressure,DateTime from sample");
+    }
+}
